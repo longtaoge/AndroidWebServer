9209deaf3b2484c15de9a69b5965c65727f6036d
longtaoge <61852263@qq.com>
2018-04-28 15:23:46 +0800

    android 起 APP 服务

--- app/src/main/java/com/mikhaellopez/androidwebserver/AndroidWebServer.java
+++ app/src/main/java/com/mikhaellopez/androidwebserver/AndroidWebServer.java
@@ -1,5 +1,9 @@
 package com.mikhaellopez.androidwebserver;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
@@ -20,12 +24,23 @@ public class AndroidWebServer extends NanoHTTPD {
     @Override
     public Response serve(IHTTPSession session) {
         String msg = "<html><body><h1>Hello server</h1>\n";
+        try {
+            session.parseBody(new HashMap<String, String>());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ResponseException e) {
+            e.printStackTrace();
+        }
+
+
         Map<String, String> parms = session.getParms();
         if (parms.get("username") == null) {
             msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
         } else {
             msg += "<p>Hello, " + parms.get("username") + "!</p>";
         }
+
+        Log.i(AndroidWebServer.class.getSimpleName(),GsonUtils.createGsonString(parms));
         return newFixedLengthResponse( msg + "</body></html>\n" );
     }
 }
